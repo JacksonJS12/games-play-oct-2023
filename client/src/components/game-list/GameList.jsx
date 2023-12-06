dbe2a432db9f51c8b6ea0564105bb8c955f4bdd1
JacksonJS12 <d.ivan0v.6100@gmail.com>
2023-12-06 21:43:21 +0200

    Add get all games service methods. Add abstract requester.

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,35 +1,51 @@
+import { useEffect, useState } from "react";
+
+import * as gameService from "../../services/gamesService";
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+        .then((result) => setGames(result));
+    }, []);
+
+    console.log(games)
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
+                    <img src="./images/avatar-1.jpg" />
                     <h6>Action</h6>
                     <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
+                    <a href="#" className="details-button">
+                        Details
+                    </a>
                 </div>
-
             </div>
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
+                    <img src="./images/avatar-1.jpg" />
                     <h6>Action</h6>
                     <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
+                    <a href="#" className="details-button">
+                        Details
+                    </a>
                 </div>
-
             </div>
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg/"/>
+                    <img src="./images/avatar-1.jpg/" />
                     <h6>Action</h6>
                     <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
+                    <a href="#" className="details-button">
+                        Details
+                    </a>
                 </div>
             </div>
 
             <h3 className="no-articles">No articles yet</h3>
         </section>
-    )
+    );
 }
--- client/src/services/gamesService.js
+++ client/src/services/gamesService.js
@@ -1,7 +1,17 @@
-const baseUrl = "localhost:3030/jsonstore/";
+import { request } from "../lib/request";
+
+const baseUrl = "http://localhost:3030/jsonstore/games";
+
+export const getAll = async () => {
+    const result = await request('GET', baseUrl)
+
+    console.log(result);0
+
+    return Object.values(result);
+};
 
 export const create = async (gameData) => {
-    const response = fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: "POST",
         headers: {
             "content-type": "application/json",
